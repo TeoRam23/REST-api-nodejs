754c1d69f4dee5fa8d86ff1c719422a1c84bed39
TeoRam23 <TeoRam23@vgs.nfk.no>
2024-04-17 11:46:01 +0200

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 80
+app.use(cors())
+
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhNmQ4ZDc2MWU3YjU0MDViODdiOGY2NGM5ZDc4MDY2NiIsImlhdCI6MTcxMzMzNzUxMSwiZXhwIjoyMDI4Njk3NTExfQ.ZlWZnPHJl8OC1gprzCbWASG5E0ORMH523rKObldaUPU'
+
+async function api_status() {
+    try {
+        const response = await fetch('http://10.100.0.130:8123/api/', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        console.log("APIet sin status er: " + response.status)
+    } catch {
+        console.error("Error, ÆÆÆ!: ")
+    }
+}
+
+
+async function get_entity(entity_id) {
+    try {
+        const response = await fetch('http://10.100.0.130:8123/api/states/' + entity_id, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        const data = await response.json()
+        return data
+    } catch {
+        console.error("Error, ÆÆÆ!: ")
+    }
+}
+
+// api_status()
+
+// get_entity('binary_sensor.shellymotion2_842E14FFCE72_motion')
+
+app.get('/sensor/:reqEntityId', async function (req, res){
+    var data = await get_entity(req.params.reqEntityId)
+    res.json({svar: data})
+})
+
+
+app.listen(PORT, (error) =>{ 
+    if(!error) 
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+    else 
+        console.log("Error occurred, server can't start", error); 
+    } 
+  ); 
---
+++ Frontend/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { ref, onMounted } from 'vue';
+const state_message = ref("State kommer her")
+const change_message = ref("Change kommer her")
+
+export default {
+  setup() {
+    async function responseEntity() {
+      try {
+        const response = await fetch('http://10.0.0.155/sensor/binary_sensor.shellymotion2_842E14FFCE72_motion');
+        const data = await response.json();
+        state_message.value = data.svar.state;
+        change_message.value = data.svar.last_changed;
+        console.log(data)
+      } catch (error) {
+          console.error('There was an error fetching the posts ÆÆÆÆ:', error);
+      };
+    }
+    onMounted(responseEntity)
+    return {
+      state_message,
+      change_message
+    }
+  }}
+
+</script>
+
+<template>
+  <h1><('o ')></h1>
+  <p>{{ state_message }}</p>
+  <p>{{ change_message }}</p>
+</template>
